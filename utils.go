2afa47d5b3a008a6f7e3dbbcac5c67656db9a7e1
siman <simanmore@yandex.ru>
2022-03-19 14:04:55 +0700

    Поменял функцию создания черного изображения `createBlackImage`. Теперь создание происходить вручную, а не функцией `Set`. Получил большой выигрыш по производительности

--- utils.go
+++ utils.go
@@ -3,25 +3,27 @@ package main
 import (
 	"bytes"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/image/bmp"
 	"image"
-	"image/color"
 	"strconv"
 )
 
 func createBlackImage(width, height int) *image.NRGBA {
-	out := image.NewNRGBA(image.Rect(0, 0, width, height))
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			out.Set(x, y, color.NRGBA{R: 0, G: 0, B: 0, A: 255})
-		}
+	buf1 := make([]uint8, height*width*4)
+	var i int64
+	for i = 3; i <= int64(height*width*4); i += 4 {
+		buf1[i] = 255
 	}
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+	img.Pix = buf1
+	img.Stride = 4 * width
 
-	return out
+	return img
 }
 
 func writeBmpIntoTheBody(fragmentImgBg *image.NRGBA, c *gin.Context) error {
 	buf := new(bytes.Buffer)
-	err := Encode(buf, fragmentImgBg)
+	err := bmp.Encode(buf, fragmentImgBg)
 	if err != nil {
 		return err
 	}
