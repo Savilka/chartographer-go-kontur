565c2113fd6cf8efda07b080601eb98ceaa079f3
siman <simanmore@yandex.ru>
2022-03-20 20:55:28 +0700

    Теперь возращает не указатель а копию

--- utils.go
+++ utils.go
@@ -8,15 +8,17 @@ import (
 	"strconv"
 )
 
-func createBlackImage(width, height int) *image.NRGBA {
-	img := image.NewNRGBA(image.Rect(0, 0, width, height))
+func createBlackImage(width, height int) image.NRGBA {
+	buf := make([]uint8, 4*width*height)
 	var i int64
 	for i = 3; i <= int64(height*width*4); i += 4 {
-		img.Pix[i] = 255
+		buf[i] = 255
 	}
-	img.Stride = 4 * width
-
-	return img
+	return image.NRGBA{
+		Pix:    buf,
+		Stride: 4 * width,
+		Rect:   struct{ Min, Max image.Point }{Min: image.Point{X: 0, Y: 0}, Max: image.Point{X: width, Y: height}},
+	}
 }
 
 func writeBmpIntoTheBody(fragmentImgBg *image.NRGBA, c *gin.Context) error {
